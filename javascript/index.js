167ce5593f6bb7ad6bcae88d95c661e12e945aa2
Christine <1603661@rgu.ac.uk>
2018-02-21 17:32:17 +0000

    external javascript added

---
+++ javascript/index.js
@@ -0,0 +1,8 @@
+var Index = function(){};
+
+Index.load = function(){
+    $('.ui.sidebar').sidebar('attach events', '#burger-menu');
+    $(".remove").off('click').on('click', function(){
+        $('.ui.sidebar') .sidebar('hide');
+    }); 
+};
