ca4afa94dd6b98ec4040a456082cbe6de6c92193
Christine Chau <1603661@rgu.ac.uk>
2018-03-19 16:11:55 +0000

    new tab system

--- javascript/index.js
+++ javascript/index.js
@@ -1,9 +1,15 @@
 var Index = function(){};
 
 Index.load = function(){
+    $("#mainContent").removeClass("lab-hidden");
     $('.ui.sidebar').sidebar('setting','closeable',false).sidebar('attach events', '#burger-menu');
     $(".remove").off('click').on('click', function(){
         $('.ui.sidebar') .sidebar('hide');
     });
     $('.ui.dropdown').dropdown();
+    $('.sidebar.inverted.vertical.menu .item').tab();
+    
 };
+Index.onChange = function (){
+    $('.vertical.pointing.menu .item').tab();
+}
