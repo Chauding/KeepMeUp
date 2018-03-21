cab7692d263e2c90b83abfcdced3b66485ee2ecb
Cameron Brown <1502390@rgu.ac.uk>
2018-03-21 11:01:53 +0000

    images

--- javascript/index.js
+++ javascript/index.js
@@ -4,7 +4,7 @@ Index.load = function(){
     $("#mainContent").removeClass("lab-hidden");
     $('.ui.sidebar').sidebar('setting','closeable',false).sidebar('attach events', '#burger-menu');
     $(".remove").off('click').on('click', function(){
-        $('.ui.sidebar') .sidebar('hide');
+        $('.ui.sidebar').sidebar('hide');
     });
     $('.ui.dropdown').dropdown();
     $('.menu .item').tab();
